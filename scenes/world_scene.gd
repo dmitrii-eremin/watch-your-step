6b1401272f7ddf04615340e69be6e983751fed80
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-12-19 00:11:15 +0200

    added level00

--- scenes/world_scene.gd
+++ scenes/world_scene.gd
@@ -7,11 +7,30 @@ extends Node2D
 @onready var _camera = $YSortedObjects/Player/PlayerCamera
 
 func _ready() -> void:
+	_initialize_camera()
+	_initialize_mushrooms()
+	_initialize_house()
+	
+func _initialize_house() -> void:
+	var houses = get_tree().get_nodes_in_group(&"house")
+	for house in houses:
+		house.player_hit.connect(_on_mushroom_house_player_hit)
+	
+func _initialize_mushrooms() -> void:
+	var mushrooms = get_tree().get_nodes_in_group(&"mushroom")
+	for mushroom in mushrooms:
+		mushroom.player_caught_mushroom.connect(_on_mushroom_player_caught_mushroom)
+		mushroom.take_damage.connect(_on_mushroom_take_damage)
+		mushroom.dead.connect(_on_mushroom_dead)
+		mushroom.collected.connect(_on_mushroom_collected)
+	
+func _initialize_camera() -> void:
 	var world_size: Rect2i = _tilemap.get_used_rect()
-	_camera.limit_left = world_size.position.x
-	_camera.limit_top = world_size.position.y
-	_camera.limit_right = world_size.size.x
-	_camera.limit_bottom = world_size.size.y
+	_camera.limit_enabled = true
+	_camera.limit_left = world_size.position.x * _tilemap.tile_set.tile_size.x
+	_camera.limit_top = world_size.position.y * _tilemap.tile_set.tile_size.y
+	_camera.limit_right = (world_size.position.x + world_size.size.x) * _tilemap.tile_set.tile_size.x
+	_camera.limit_bottom = (world_size.position.y + world_size.size.y) * _tilemap.tile_set.tile_size.y
 
 func _on_mushroom_player_caught_mushroom(player: Node2D, mushroom: Node2D) -> void:
 	player.add_mushroom(mushroom)
