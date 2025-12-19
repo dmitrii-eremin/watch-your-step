12e6b82f28553c03d0aad82ea4d4cf69aac8f3aa
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-12-19 13:51:44 +0200

    added transition scene + shader

--- scenes/world_scene.gd
+++ scenes/world_scene.gd
@@ -64,7 +64,10 @@ func _on_virtual_joystick_on_joystick_input(direction: Vector2) -> void:
 	_player.update_virtual_joystick(direction)
 
 func _level_completed() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://scenes/levels/menu_level.tscn")
+	_change_level_scene("res://scenes/levels/menu_level.tscn")
 
 func _on_player_dead() -> void:
 	pass
+
+func _change_level_scene(path: String) -> void:
+	Transition.change_scene(path)
