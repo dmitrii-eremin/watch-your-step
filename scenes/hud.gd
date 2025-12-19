0b7aabf22e4e39635caa8324a1e1476549a659de
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-12-19 13:16:46 +0200

    hide stats in menu level

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,7 +1,8 @@
 extends CanvasLayer
 
-@onready var _mushrooms_count_label: Label = $MarginContainer/Control/VBoxContainer/HBoxContainer/CaughtMushroomsLabel
-@onready var _saved_mushrooms_label: Label = $MarginContainer/Control/VBoxContainer/HBoxContainer2/SavedMushroomsLabel
+@onready var _stats_container: VBoxContainer = $MarginContainer/Control/StatsVBoxContainer
+@onready var _mushrooms_count_label: Label = $MarginContainer/Control/StatsVBoxContainer/HBoxContainer/CaughtMushroomsLabel
+@onready var _saved_mushrooms_label: Label = $MarginContainer/Control/StatsVBoxContainer/HBoxContainer2/SavedMushroomsLabel
 
 var _collected_mushrooms_count: int = 0
 var _original_mushrooms_count: int = 0
@@ -14,6 +15,9 @@ func update_mushrooms() -> void:
 	var all_mushrooms: Array = get_tree().get_nodes_in_group("mushroom")
 	var caught_mushrooms: Array = all_mushrooms.filter(func(m): return m.is_caught())
 	_mushrooms_count_label.text = "%d/%d" % [caught_mushrooms.size(), all_mushrooms.size()]
+	
+func show_stats(visibility: bool) -> void:
+	_stats_container.visible = visibility
 	
 func _ready() -> void:
 	_original_mushrooms_count = get_tree().get_nodes_in_group("mushroom").size()
--- scenes/menu_level.gd
+++ scenes/menu_level.gd
@@ -1,10 +1,11 @@
 extends "res://scenes/world_scene.gd"
 
-@onready var original_mushroom_house = $YSortedObjects/MushroomHouse
+@onready var _original_mushroom_house = $YSortedObjects/MushroomHouse
 
 func _ready() -> void:
 	_initialize_camera()
-	original_mushroom_house.call_deferred("queue_free")
+	_hud.show_stats(false)
+	_original_mushroom_house.call_deferred("queue_free")
 
 func _on_start_game(_point: Node2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level_03.tscn")
