c2aa450f5b172cda4ab61861513199f2e6171e27
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-12-19 12:50:25 +0200

    added secret npc

---
+++ scenes/secret_npc.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const _not_ready_yet: String = "You are not ready yet.\nCome back later."
+
+@onready var _label: Label = $Label
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		_label.text = _not_ready_yet
+		_label.visible = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		_label.visible = false
