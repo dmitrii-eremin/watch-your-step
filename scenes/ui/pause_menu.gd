b6a96dc044c8d5cdc768592dd375b1abf40db86e
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-12-20 19:01:36 +0200

    escape should unpause

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -8,6 +8,11 @@ func pause() -> void:
 
 func _ready() -> void:
 	set_process_input(true)
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_on_resume_button_pressed()
+		get_viewport().set_input_as_handled()
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
