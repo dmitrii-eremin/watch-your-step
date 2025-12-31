b308b9a4ff8789cd6c9a8698cf7234e07a090780
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-12-31 20:13:10 +0200

    added cat

--- scenes/secret_npc.gd
+++ scenes/secret_npc.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-const _not_ready_yet: String = "I am not ready yet\nCome back later."
+@export var npc_text: String = "I am not ready yet\nCome back later."
 
 @onready var _label: Label = $Label
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		_label.text = _not_ready_yet
+		_label.text = npc_text.replace("\\n", "\n")
 		_label.visible = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
