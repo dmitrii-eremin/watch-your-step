57effaa1cd24944e211612bc1439b26b615b136c
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2026-01-02 22:35:00 +0200

    fix select level

--- scenes/levels/select_level.gd
+++ scenes/levels/select_level.gd
@@ -1,6 +1,7 @@
 extends "res://scenes/world_scene.gd"
 
 @onready var _original_mushroom_house = $YSortedObjects/MushroomHouse
+@onready var _back_to_main_menu_house := $YSortedObjects/Houses/HouseBack
 
 func _ready() -> void:
 	_initialize_camera()
@@ -13,6 +14,7 @@ func _update_house_labels() -> void:
 	for house in houses:
 		house.open_door(false)
 		_update_house_label(house)
+	_back_to_main_menu_house.open_door(true)
 
 func _update_house_label(house: Node2D) -> void:
 	if not house.has_meta(&"scene_name"):
@@ -50,7 +52,6 @@ func _convert_time_spent_to_string(value: int) -> String:
 	var seconds = value % 60
 	return "%02d:%02d" % [minutes, seconds]
 
-
 func _on_house_player_hit(_point: Node2D, scene_name: String) -> void:
 	var found_level_name: String = ""
 	for level_name in Globals.LEVELS.keys():
@@ -64,3 +65,6 @@ func _on_house_player_hit(_point: Node2D, scene_name: String) -> void:
 		return
 	var target_scene = "res://scenes/levels/%s.tscn" % [scene_name]
 	Transition.change_scene(target_scene)
+
+func _on_house_back_player_hit(_point: Node2D) -> void:
+	Transition.change_scene("res://scenes/levels/menu_level.tscn")
